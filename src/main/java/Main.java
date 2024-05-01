d2edb4b86dbc7b703d9b3b4cf452685455945e01
dirtyphonicsss@yandex.ru <dirtyphonicsss@yandex.ru>
2024-05-01 15:42:43 +0300

    changes in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ import java.util.concurrent.RecursiveTask;
 
 
 public class Main {
-
+//    private static final ConcurrentSkipListSet<String> links = new ConcurrentSkipListSet<>();
 
     public static void main(String[] args) throws IOException {
 
@@ -26,68 +26,10 @@ public class Main {
         Set<String> result = new HashSet<>(pool.invoke(myFork));
 
         pool.shutdown();
-        result.forEach(System.out::println);
+//        result.forEach(System.out::println);
 
 
     }
-
-
-    static class MyFork extends RecursiveTask<ConcurrentSkipListSet<String>> {
-        ConcurrentSkipListSet<String> links = new ConcurrentSkipListSet<>();
-
-        private String link;
-
-
-        public MyFork(String link) {
-            this.link = link;
-        }
-
-        @SneakyThrows
-        @Override
-        protected ConcurrentSkipListSet<String> compute() {
-            Thread.sleep(500);
-            Set<MyFork> tasks = new HashSet<>();
-
-            String regex = "https?://[^,\\s]+";
-
-            Document doc = Jsoup.connect(link)
-                    .userAgent("Mozilla").get();     // .timeout(3000)
-            Elements elements = doc.select("a");
-
-            for (Element element : elements) {
-                String newLink = element.attr("abs:href");
-                boolean checkLink = newLink.matches(regex) &&
-                        newLink.contains(getDomen(link)) &&
-                        !links.contains(newLink);
-
-                if (checkLink) {
-                    links.add(newLink);
-                    continue;
-                }
-                if (checkLink) {
-                    MyFork myFork = new MyFork(newLink);
-                    myFork.fork();
-                    tasks.add(myFork);
-                }
-
-            }
-
-            tasks.forEach(task -> {
-                task.join();
-            });
-            return links;
-
-        }
-    }
-
-    public static String getDomen(String url) {
-        String domen = (url.contains("www")) ?
-                url.substring(12).split("/", 2)[0] : url.substring(8).split("/", 2)[0];
-
-
-        return domen;
-    }
-
 
 }
 
---
+++ src/main/java/MyFork.java
@@ -0,0 +1,74 @@
+import lombok.SneakyThrows;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.RecursiveTask;
+
+
+public class MyFork extends RecursiveTask<ConcurrentSkipListSet<String>> {
+
+    private static ConcurrentSkipListSet<String> links = new ConcurrentSkipListSet<>();
+    private String link;
+
+
+    public MyFork(String link) {
+        this.link = link;
+    }
+
+    @SneakyThrows
+    @Override
+    protected ConcurrentSkipListSet<String> compute() {
+
+        Thread.sleep(500);
+        Set<MyFork> tasks = new HashSet<>();
+
+        String regex = "https?://[^,\\s]+";
+
+        Document doc = Jsoup.connect(link)
+                .userAgent("Mozilla").get();     // .timeout(3000)
+        Elements elements = doc.select("a");
+
+        for (Element element : elements) {
+            String newLink = element.attr("abs:href");
+            boolean checkLink = newLink.matches(regex) &&
+                    newLink.contains(getDomen(link)) &&
+                    !links.contains(newLink) &&
+                    !newLink.contains(".pdf") &&
+                    !newLink.contains(".jpg");
+
+
+            if (checkLink) {
+                links.add(link);
+                System.out.println(link);
+                MyFork myFork = new MyFork(newLink);
+                myFork.fork();
+                tasks.add(myFork);
+
+            }
+
+        }
+
+        tasks.forEach(task -> {
+            task.join();
+        });
+        return links;
+
+    }
+    public static String getDomen(String url) {
+        String domen = (url.contains("www")) ?
+                url.substring(12).split("/", 2)[0] : url.substring(8).split("/", 2)[0];
+
+
+        return domen;
+    }
+
+
+}
+
+
+
